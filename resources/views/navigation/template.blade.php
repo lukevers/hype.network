97d161cd1e24b91dc4dc55bcda2e409760f1158d
Luke Evers <me@lukevers.com>
2015-07-19 09:06:58 -0400

    Name the navigation id `#navigation`

--- resources/views/navigation/template.blade.php
+++ resources/views/navigation/template.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -9,7 +9,7 @@
             </button>
         </div>
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse" id="navigation">
             @yield('nav')
         </div>
     </div>
